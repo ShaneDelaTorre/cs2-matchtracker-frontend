d1cbd8491ea8ef622cd382c72288e1a2fa5d2079
Shane <jc.delatorre@academy.opswerks.com>
2026-04-19 02:17:03 +0800

    feat(layout): add new layout for auth route group

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import styles from "./layout.module.css"
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={styles.shell}>
+      <div className={styles.box}>{children}</div>
+    </div>
+  )
+}
