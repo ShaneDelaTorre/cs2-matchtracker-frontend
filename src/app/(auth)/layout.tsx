9a77d6752b023b1d543b0528538684366342851c
Shane <jc.delatorre@academy.opswerks.com>
2026-04-19 23:58:53 +0800

    chore(auth-pages-layout): update layout format using formatter package

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,13 @@
-import styles from "./layout.module.css"
+import styles from "./layout.module.css";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className={styles.shell}>
       <div className={styles.box}>{children}</div>
     </div>
-  )
+  );
 }
